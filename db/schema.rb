ef14f34301786404e69302658d3013f8fffaf783
brandon <brandon.ngongolo@student.moringaschool.com>
2023-05-01 20:15:06 -0400

    created user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_234543) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_001255) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "avatar_url"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
